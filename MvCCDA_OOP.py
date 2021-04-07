b4b0fda5e4e467155a11bf8523d3e83bb6b85b19
IndefiniteCalculus <55014395+IndefiniteCalculus@users.noreply.github.com>
2021-04-07 14:47:08 +0800

    fix bugs and some upgrade about obtain_class, get onehot and similarity mat methods that located in MvCCDA_OOP.py

--- MvCCDA_OOP.py
+++ MvCCDA_OOP.py
@@ -4,31 +4,33 @@ from scipy.linalg import eigh
 
 
 class MvCCDA():
-    def __init__(self, alpha = 2.3849, lambda1 = 0.6,lambda2 = 0.002,lambda3 = 0.0005,sigma = 2000,subspace_dim = 45):
-
+    def __init__(self, alpha=2.3849, lambda1=0.6, lambda2=0.002, lambda3=0.0005,
+                 sigma=2000, subspace_dim=45, algorithm="LPP", lambda4=None, t=None):
         # initial parameters of hyper setting
-        # hyperparameters = {"alpha": alpha, "lambda1": lambda1, "lambda2": lambda2, "lambda3": lambda3, "sigma": sigma,
-        #                    "subspace_dim": subspace_dim}
         self.alpha = alpha
         self.lambda1 = lambda1
         self.lambda2 = lambda2
         self.lambda3 = lambda3
         self.sigma = sigma
         self.subspace_dim = subspace_dim
+        self.lambda4 = lambda4
+        self.t = t  # the coefficient of Sb-tSw
+        self.algorithm = algorithm
+
+    def train(self, train_data, train_labels,
+              common_comp: np.array = None, map_matrices: list = None, rand_seed=None):
+        # TODO: add a method to recompile labels when input labels into train method. Make sure the compiled labels
+        #  only contain continuous integers started with 1 (get onehot method can handle such start)
 
 
-        # self.data_param = {"num_sample": num_sample, "num_dim": num_dim, "num_view": num_view}
-
-
-    def train(self, train_data,
-              common_comp:np.array = None, map_matrices:list = None, rand_seed = None):
         # initial parameters about input data
         self.num_sample = self._obtain_numsample(train_data[0])
         self.num_dim = self._obtain_numdim(train_data[0])
         self.num_view = self._obtain_numview(train_data)
+        self.num_class, self.max_class, self.min_class = self._obtain_numclass(train_labels)
         # initial matrices which will be used in the update procedures
-        onehots, _ = self._num2onehot(labels)
-        hotkernel_mat = self._get_hotkernel_mat(labels, train_data)
+        onehots = self._num2onehot(train_labels)
+        hotkernel_mat = self._get_hotkernel_mat(train_labels, train_data)
 
         if rand_seed is not None:
             np.random.seed(rand_seed)
@@ -78,19 +80,36 @@ class MvCCDA():
                         Q.append(1 / (np.dot(r[v], r[v].T) + self.alpha ** 2))
                     sum_Q = sum(Q)
 
-                    # assemble the two part of the objective function
-                    a = self.num_view * self.lambda2 \
-                        + self.num_view * self.lambda3 * weight_map_i \
-                        + sum_Q
+                    # assemble the two part of the objective function a and b(object = a^-1 * b)
+                    if self.algorithm == "LPP":
+                        # the assemble of 'a' should be
+                        a = self.num_view * self.lambda2 \
+                            + self.num_view * self.lambda3 * weight_map_i \
+                            + sum_Q
 
-                    b = self.num_view * self.lambda2 * onehots.get("train")[0][i,
-                                                                :] + self.num_view * self.lambda3 * weighted_comp_i
-                    for v in range(self.num_view):
-                        b = b + Q[v] * np.dot(map_matrices[v], train_data[v][i, :])
+                        # the assemble of 'b' should be
+                        b = self.num_view * self.lambda2 * onehots[0][i, :] \
+                            + self.num_view * self.lambda3 * weighted_comp_i
+                        for v in range(self.num_view):
+                            b = b + Q[v] * np.dot(map_matrices[v], train_data[v][i, :])
+                        updated_common_comp_i = np.linalg.inv(np.eye(self.subspace_dim) * a).dot(b.reshape(-1, 1))
+
+                    if self.algorithm == "LPDP":
+                        # the LPDP require an extra adjacency matrix to describe Sw and Sb(replace as St - Sw)
+
+                        # the assemble of 'a' should be
+                        a = self.num_view * self.lambda2 \
+                            + self.num_view * self.lambda3 * weight_map_i \
+                            + sum_Q
+
+                        # the assemble of 'b' should be
+                        b = self.num_view * self.lambda2 * onehots[0][i, :] \
+                            + self.num_view * self.lambda3 * weighted_comp_i
+                        for v in range(self.num_view):
+                            b = b + Q[v] * np.dot(map_matrices[v], train_data[v][i, :])
+                        updated_common_comp_i = np.linalg.inv(np.eye(self.subspace_dim) * a).dot(b.reshape(-1, 1))
 
                     # estimate loss of this component and break the loop if coverage
-                    updated_common_comp_i = np.linalg.inv(np.eye(self.subspace_dim) * a).dot(
-                        b.reshape(-1, 1))
                     loss = updated_common_comp_i.reshape(1, -1) - common_comp[i, :]
                     loss = np.dot(loss, loss.T)
                     loss = np.sqrt(loss)
@@ -103,12 +122,13 @@ class MvCCDA():
                     if optimized_count > 200:
                         coveraged = False
                         break
+
             # optimize map matrix based on common_component
             optimized_count = 0
             coveraged = None
 
-            # update component in view width
-            last_map_matrixes = tuple(map_matrices)
+            # update map matrices in view width
+            last_map_matrices = tuple(map_matrices)
             for v in range(self.num_view):
                 map_mat_v = map_matrices[v]
                 # optimize mapping matrix on view v
@@ -123,20 +143,17 @@ class MvCCDA():
                         G.append(1 / (np.dot(r_i, r_i.T) + self.alpha ** 2))
                     data_X = train_data[v]
                     G = np.array(G).reshape(-1, 1)
-                    # assuming the structure of objective function should be like b / a,
-                    # the assemble of 'a' should be
                     XG = np.dot(G * np.eye(self.num_sample),
                                 data_X)
                     XGX = np.dot(data_X.T, XG)
-                    a = XGX + self.lambda1 * self.num_sample * np.eye(
-                        self.num_dim)
-                    # the assemble of 'b' should be
-                    # b = np.dot(common_comp.T, XG)
-                    GZ = np.dot(G * np.eye(self.num_sample),
-                                common_comp)
+                    # assume the structure of objective function should be like a^-1 * b,
+                    # the assemble of 'a' should be
+                    a = XGX + self.lambda1 * self.num_sample * np.eye(self.num_dim)
+                    # the assemble of 'b' should be XGZ
+                    # GZ = np.dot(G * np.eye(self.num_sample),
+                    #             common_comp)
                     b = np.dot(common_comp.T, XG)
                     updated_map_mat_v = np.dot(np.linalg.inv(a), b.T).T
-
                     # use Frobenius norm to estimate loss of mapping matrix
                     loss = updated_map_mat_v - map_mat_v
                     loss = loss * loss
@@ -160,7 +177,7 @@ class MvCCDA():
             # now estimate the max loss of map matrixes
             max_loss = 0
             for map_id in range(len(map_matrices)):
-                loss = last_map_matrixes[map_id] - map_matrices[map_id]
+                loss = last_map_matrices[map_id] - map_matrices[map_id]
                 loss = loss * loss
                 loss = np.sqrt(np.sum(loss))
                 max_loss = max(loss, max_loss)
@@ -205,11 +222,9 @@ class MvCCDA():
         acc_ave = sum(acc_list) / len(acc_list) * 100
         return acc_ave
 
-    def _get_hotkernel_mat(self, label_set, train_data):
-        _, max_l, min_l = self._obtain_numclass(label_set)
+    def _get_hotkernel_mat(self, label, train_data):
         # obtain similarity matrix on multi_view
         Sv = None
-        label = label_set.get("train")
         for view_label, view_data in zip(label, train_data):
             # for each view
             similarity_mat = np.zeros((self.num_sample, self.num_sample))
@@ -232,37 +247,33 @@ class MvCCDA():
                     Sv[i, j] = 0
         return Sv
 
-    def _num2onehot(self, label_set):
+    def _num2onehot(self, labels):
         # 以(num_sample, num_classes)的格式组织独热编码，假设训练数据都包含了所有的类
-        num_classes, max_l, min_l = self._obtain_numclass(label_set)
-        v_labels = {}
-        for data_type in label_set.keys():
-            v_labels.update({data_type: None})
-        for data_type in label_set.keys():
-            labels = label_set.get(data_type)
-            onehots = []
-            for view in labels:
-                onehot = np.zeros((view.shape[1], num_classes))
-                for dim1 in range(view.shape[1]):
-                    l = view[0, dim1] - min_l
-                    onehot[dim1, l] = 1
-                onehots.append(onehot)
-            v_labels.update({data_type: onehots})
-        pass
-        return v_labels, num_classes
+        onehots = []
+        for view in labels:
+            onehot = np.zeros((max(view.shape), self.num_class))
+            for dim1 in range(max(view.shape)):
+                l = view[0, dim1] - self.min_class
+                onehot[dim1, l] = 1
+            onehots.append(onehot)
+        return onehots
 
     def _obtain_numclass(self, label_set):
-        # obtain number of class in the label_set. Assuming all classes have been present in the label_set
-        min_l, max_l = float("inf"), 0
+        # obtain number of class in the label_set.
+        # assume all classes have been present in the label_set
+        # assume all the labels from each view was aligned based on same data object it belongs to
         # obtain max label and min label among all dataset
-        for data_type in label_set.keys():
-            temp_min, temp_max = np.min(label_set.get(data_type)), \
-                                 np.max(label_set.get(data_type))
-            if temp_max > max_l:
-                max_l = temp_max
-            if temp_min < min_l:
-                min_l = temp_min
+        min_l, max_l = np.min(label_set[0]), \
+                       np.max(label_set[0])
         num_classes = max_l - min_l + 1
+        # annotation this code,
+        # To do: add a method  to recompile label when input labels into train method to make sure labels are distinct
+        # # make sure the num_classes is about the number of distinct label among the label set
+        # bitmap = np.ones(num_classes)
+        # for l_id in range(max(label_set[0].shape)):
+        #     bitmap[label_set[0][0,l_id]-min_l] = 0
+        # idices = bitmap[bitmap == 1].tolist()
+        # num_classes = np.sum(bitmap)
         return num_classes, max_l, min_l
 
     def _obtain_numsample(self, data):
@@ -277,6 +288,7 @@ class MvCCDA():
         # obtain number of view the dataset have
         return len(data_set)
 
+
 if __name__ == "__main__":
     train_data, test_data, labels = dataloader.load_data("matlabpca")
 
@@ -288,7 +300,8 @@ if __name__ == "__main__":
         map_matrixes[m_idx] = map_matrixes[m_idx].T
 
     model = MvCCDA()
-    map_matrixes, common_comp = model.train(train_data, common_comp, map_matrixes)
+    train_labels = labels.get("train")
+    map_matrixes, common_comp = model.train(train_data, train_labels, common_comp, map_matrixes)
     pass
     # map_matrixes, common_comp = train(train_data, map_matrixes, common_comp, onehots, hotkernel_mat, train_data_param, hyperparameters)
     # project test data to subspace
