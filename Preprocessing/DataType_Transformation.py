16b75c3253634dcd96a5b629d04564af2b7a591e
IndefiniteCalculus <55014395+IndefiniteCalculus@users.noreply.github.com>
2021-04-12 02:24:48 +0800

    add mnist-usps transform_mnist-usps.py

--- Preprocessing/DataType_Transformation.py
+++ Preprocessing/DataType_Transformation.py
@@ -18,6 +18,23 @@ def save_pydata2mat(data:list, swap_dir):
     data_dict={"data":mat, "num_view":num_view}
     io.savemat(swap_dir + "\\temp.mat", data_dict)
 
+def load_pca(dataset_name):
+    dataset_name = "pie"
+    train_data, test_data, label = dataloader.load_data(dataset_name)
+    # 将数据保存为mat文件
+    save_pydata2mat(train_data, "E:\\Works\\数据集")
+    # 运行matlab代码对数据进行转换，覆写temp.mat
+    engine = matlab.engine.start_matlab()
+    engine.preprocess(nargout=0)
+    # 从matlab存储的文件中读出数据矩阵
+    pca_data = io.loadmat("E:\\Works\\数据集" + "\\temp.mat")
+    pca_data = pca_data.get('MvData')
+    num_view = pca_data.shape[1]
+    Mv_PCA = []
+    for view_idx in range(num_view):
+        Mv_PCA.append(pca_data[0, view_idx])
+    return Mv_PCA
+
 if __name__ == "__main__":
     dataset_name = "pie"
     train_data, test_data, label = dataloader.load_data(dataset_name)
@@ -28,4 +45,9 @@ if __name__ == "__main__":
     engine.preprocess(nargout=0)
     # 从matlab存储的文件中读出数据矩阵
     pca_data = io.loadmat("E:\\Works\\数据集" + "\\temp.mat")
+    pca_data = pca_data.get('MvData')
+    num_view = pca_data.shape[1]
+    Mv_pca = []
+    for view_idx in range(num_view):
+        Mv_pca.append(pca_data[0,view_idx])
     pass
---
+++ transform_mnist-usps.py
@@ -0,0 +1,56 @@
+import cv2 as cv
+import numpy as np
+import re
+import os
+
+
+def readin_flatten(root_dir, sub_dir):
+    path = root_dir + "\\" + sub_dir + "\\"
+    labels = []
+    l = int(sub_dir)
+    file_list = os.listdir(path)
+    im = cv.imdecode(np.fromfile(path + file_list[0], dtype=np.uint8), -1)
+    data_mat = np.zeros((100, im.shape[0] * im.shape[1]))
+
+    for i in range(100):
+        im = cv.imdecode(np.fromfile(path + file_list[i], dtype=np.uint8), -1)
+        vec = im.reshape(1, -1)
+        labels.append(l)
+        data_mat[i, :] = vec
+
+    return data_mat, labels
+
+
+def do_transform(root_dir):
+    # read in image in numpy array
+    # consider file name as label
+    file_list = os.listdir(root_dir)
+    Mv_Data = []
+    labels = []
+
+    pattern = re.compile('\d')
+    for f_name in file_list:
+        data_list = []
+        data = None
+        label = []
+        view_dir = root_dir + "\\" + f_name
+        digit_list = os.listdir(view_dir)
+        for d_name in digit_list:
+            if pattern.match(d_name):
+                data_mat, temp_l = readin_flatten(view_dir, d_name)
+                data_list.append(data_mat)
+                label = label + temp_l
+        # combine datalist to numpy matrix
+        for data_mat in data_list:
+            if data is None:
+                data = data_mat
+            else:
+                data = np.concatenate((data, data_mat),axis=0)
+
+        Mv_Data.append(data)
+        labels.append(label)
+    return Mv_Data, labels
+
+
+if __name__ == "__main__":
+    do_transform("E:\\Works\\数据集\\mnist-usps")
